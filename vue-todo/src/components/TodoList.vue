92c999007aedc3830e904571d22aa0ab00a73fb8
jihong <rlawyd9@kookmin.ac.kr>
2022-12-30 01:32:46 +0900

    feat: TodoList_draw list function

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>input</div>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        {{ todoItem }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      todoItems: [],
+    };
+  },
+  created: function () {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(localStorage.key(i));
+          console.log(this.todoItems);
+        }
+      }
+    }
+  }, // 인스턴스가 생성되자마자 호출
+};
 </script>
 
 <style></style>
