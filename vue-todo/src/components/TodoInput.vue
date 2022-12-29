c4899b12f120c683e317fbab1972ebee842f3ca0
jihong <rlawyd9@kookmin.ac.kr>
2022-12-29 23:45:37 +0900

    feat: TodoInput_addTodo function

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>input</div>
+  <div>
+    <input type="text" v-model="newTodoInput" />
+    <button v-on:click="addTodo">Add</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      newTodoInput: "",
+    };
+  },
+
+  methods: {
+    addTodo: function () {
+      console.log(this.newTodoInput);
+      localStorage.setItem(this.newTodoInput, this.newTodoInput);
+      this.clearInput();
+    },
+    clearInput: function () {
+      this.newTodoInput = "";
+    },
+  },
+};
 </script>
 
 <style></style>
