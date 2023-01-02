b0e124ceef2f9ec300928e254e8b33b7e8fbf5ff
jihong <rlawyd9@kookmin.ac.kr>
2023-01-03 02:27:26 +0900

    fix: modal component

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,16 +1,17 @@
-<template v-slot:header>
+<template>
   <div class="inputBox">
     <input type="text" v-model="newTodoInput" v-on:keyup.enter="addTodo" />
     <span class="addContainer" v-on:click="addTodo">
       <i class="fas fa-plus addBtn"></i>
     </span>
-
     <AlertModal v-if="showModal" @close="showModal = false">
       <!--
     you can use custom content here to overwrite
     default content
     -->
-      <h3 slot:header>경고</h3>
+      <template v-slot:header>
+        <h3>경고</h3>
+      </template>
     </AlertModal>
   </div>
 </template>
@@ -38,9 +39,9 @@ export default {
     clearInput: function () {
       this.newTodoInput = "";
     },
-    components: {
-      AlertModal: AlertModal,
-    },
+  },
+  components: {
+    AlertModal: AlertModal,
   },
 };
 </script>
